fffa24145a53f4f53bbc64568b0f188809a73b05
Semya <kartelvs@bk.ru>
2026-03-14 08:50:28 +0300

    newCommit

---
+++ src/model/TourService.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public abstract class TourService {
+    private Integer id;
+    private String name;
+    private BigDecimal price;
+    private LocalDate from;
+    private LocalDate to;
+
+    public boolean isAvailableOn(LocalDate date){
+        return !(from.isAfter(date) || to.isBefore(date));
+    }
+
+    public abstract BigDecimal calculateTotalPrice(int participants);
+
+}
